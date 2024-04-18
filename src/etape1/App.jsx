1b1b929d948433bd29e1e098980836f88f917b3d
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-04-18 06:57:52 +0200

    Utilisation d'une enum pour le statut

--- src/etape1/App.jsx
+++ src/etape1/App.jsx
@@ -1,10 +1,11 @@
 import Task from "./components/Task"
+import { TaskStatus } from "../taskStatus";
 
 function App() {
   return (
     <>
       <h1 className="center-text">Gestion des tâches  (étape 1)</h1>
-      <Task id={1} totalTime="100" timeToComplete="70" status="En cours" taskName="Première tâche"/>
+      <Task id={1} totalTime="100s" timeToComplete="70s" status={TaskStatus.IN_PROGRESS} taskName="Première tâche"/>
     </>
   )
 }
---
+++ src/taskStatus.js
@@ -0,0 +1,6 @@
+export const TaskStatus = Object.freeze({
+  NOT_STARTED: Symbol("not_started"),
+  IN_PROGRESS: Symbol("in_progress"),
+  PAUSED: Symbol("paused"),
+  COMPLETED: Symbol("completed")
+});
