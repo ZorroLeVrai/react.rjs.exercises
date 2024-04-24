74784639730d6c0f813c45365464c163a1e66120
ZorroLeVrai <69362591+ZorroLeVrai@users.noreply.github.com>
2024-04-24 23:51:31 +0200

    Add zod constraints

--- src/etape3/components/TaskForm.jsx
+++ src/etape3/components/TaskForm.jsx
@@ -3,9 +3,26 @@ import styles from "./TaskForm.module.css";
 import { idGenerator, statusMap } from '../tools';
 import { TaskStatus } from '../../taskStatus';
 import PropTypes from 'prop-types';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const datePattern = /(\d+d)?\s+(\d+h)?\s+(\d+m)?\s+(\d+s)?/;
+
+const schema = z.object({
+  taskName: z.string({required_error: "champ requis"})
+    .min(1, "Saisissez le nom de la tâche"),
+  totalTime: z.string({required_error: "champ requis"})
+    .min(1, "Saisissez le temps total")
+    .refine(value => datePattern.test(value ?? ""), "Le temps total doit être au format '1d 2h 3m 4s'"),
+  remainingTime: z.string({required_error: "champ requis"})
+    .min(1, "Saisissez le temps restant")
+    .refine(value => datePattern.test(value ?? ""), "Le temps restant doit être au format '1d 2h 3m 4s'"),
+  taskStatus: z.string({required_error: "champ requis"})
+    .min(1, "Sélectionnez un status pour la tâche")
+});
 
 const TaskForm = ({handleCancel, handleFormSubmit}) => {
-  const { register, handleSubmit, formState: {errors, isValid} } = useForm();
+  const { register, handleSubmit, formState: {errors, isValid} } = useForm({resolver: zodResolver(schema)});
 
   const submitHandler = formData => {
     console.log("submitHandler");
@@ -24,9 +41,7 @@ const TaskForm = ({handleCancel, handleFormSubmit}) => {
     //handleFormSubmit(myTask);
   };
 
-  console.log({errors});
-
-  const datePattern = /(\d+d)?\s+(\d+h)?\s+(\d+m)?\s+(\d+s)?/;
+  console.log({errors});  
 
   return (
     <form className="form-margin" onSubmit={handleSubmit(submitHandler)}>
@@ -40,11 +55,12 @@ const TaskForm = ({handleCancel, handleFormSubmit}) => {
         <input {...register("remainingTime", {required: true, pattern: datePattern})} id="task-remaining-time" type="text" />
         <label htmlFor="task-status">Statut de la tâche</label>
         <select {...register("taskStatus", {required: true, validate: value => value !== "None"})} id="task-status" >
-          <option value="None">Sélectionnez un statut...</option>
+          <option value="">Sélectionnez un statut...</option>
           {Array.from(statusMap).map((statusItem, index) => <option key={index} value={statusItem[0].description}>{statusItem[1]}</option>)}
         </select>
       </div>
       {errors.taskName && <p>{errors.taskName.message}</p>}
+      {errors.totalTime && <p>{errors.totalTime.message}</p>}
       <div className="flexSpaceBetween">
         <div className={styles.buttonVerticalMargin}>
           <input type="reset" onClick={()=>console.log("reset")}/>
